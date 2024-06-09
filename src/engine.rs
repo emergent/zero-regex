5e1a5921c1b8bb8c728400d905ff15dfc205c5f4
Satoshi Yoshikawa <yoshikawa@fairydevices.jp>
2024-06-09 12:39:58 +0900

    initial commit

---
+++ src/engine.rs
@@ -0,0 +1,48 @@
+use crate::helper::DynError;
+use std::fmt::Display;
+
+mod codegen;
+mod evaluator;
+mod parser;
+
+#[derive(Debug)]
+pub enum Instruction {
+    Char(char),
+    Match,
+    Jump(usize),
+    Split(usize, usize),
+}
+
+impl Display for Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Instruction::Char(c) => write!(f, "char {c}"),
+            Instruction::Match => write!(f, "match"),
+            Instruction::Jump(addr) => write!(f, "jump {:>04}", addr),
+            Instruction::Split(addr1, addr2) => write!(f, "split {:>04}, {:>04}", addr1, addr2),
+        }
+    }
+}
+
+pub fn print(expr: &str) -> Result<(), DynError> {
+    println!("expr: {expr}");
+    let ast = parser::parse(expr)?;
+    println!("AST: {:?}", ast);
+
+    println!();
+    println!("code:");
+    let code = codegen::get_code(&ast)?;
+    for (n, c) in code.iter().enumerate() {
+        println!("{:>04}: {c}", n);
+    }
+
+    Ok(())
+}
+
+/// 正規表現と文字列をマッチング
+pub fn do_matching(expr: &str, line: &str, is_depth: bool) -> Result<bool, DynError> {
+    let ast = parser::parse(expr)?;
+    let code = codegen::get_code(&ast)?;
+    let line = line.chars().collect::<Vec<char>>();
+    Ok(evaluator::eval(&code, &line, is_depth)?)
+}
---
+++ src/engine/evaluator.rs
@@ -0,0 +1,86 @@
+use super::Instruction;
+use crate::helper::safe_add;
+use std::{
+    //collections::VecDeque,
+    error::Error,
+    fmt::{self, Display},
+};
+
+#[derive(Debug)]
+pub enum EvalError {
+    PCOverFlow,
+    SPOverFlow,
+    InvalidPC,
+    //InvalidContext,
+}
+
+impl Display for EvalError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "EvalError: {:?}", self)
+    }
+}
+
+impl Error for EvalError {}
+
+/// 深さ優先探索で再帰的にマッチンをを行う関数
+fn eval_depth(
+    inst: &[Instruction],
+    line: &[char],
+    mut pc: usize,
+    mut sp: usize,
+) -> Result<bool, EvalError> {
+    loop {
+        let next = if let Some(i) = inst.get(pc) {
+            i
+        } else {
+            return Err(EvalError::InvalidPC);
+        };
+
+        match next {
+            Instruction::Char(c) => {
+                if let Some(sp_c) = line.get(sp) {
+                    if c == sp_c {
+                        safe_add(&mut pc, &1, || EvalError::PCOverFlow)?;
+                        safe_add(&mut sp, &1, || EvalError::SPOverFlow)?;
+                    } else {
+                        return Ok(false);
+                    }
+                } else {
+                    return Ok(false);
+                }
+            }
+            Instruction::Match => {
+                return Ok(true);
+            }
+            Instruction::Jump(addr) => {
+                pc = *addr;
+            }
+            Instruction::Split(addr1, addr2) => {
+                if eval_depth(inst, line, *addr1, sp)? || eval_depth(inst, line, *addr2, sp)? {
+                    return Ok(true);
+                } else {
+                    return Ok(false);
+                }
+            }
+        }
+    }
+}
+
+/// 命令列の評価を行う関数
+///
+/// instが命令列となり、その命令列を用いて入力文字列lineにマッチさせる
+/// is_depthがtrueの場合に深さ優先探索を、falseの場合に幅湯煎探索をおkの合う
+///
+/// 実行時にエラーが起きた場合はErrを返す
+/// マッチ成功時にはOk(true)を、失敗時はOk(false)を返す
+pub fn eval(inst: &[Instruction], line: &[char], is_depth: bool) -> Result<bool, EvalError> {
+    if is_depth {
+        eval_depth(inst, line, 0, 0)
+    } else {
+        eval_width(inst, line)
+    }
+}
+
+pub fn eval_width(_inst: &[Instruction], _line: &[char]) -> Result<bool, EvalError> {
+    Ok(false)
+}
